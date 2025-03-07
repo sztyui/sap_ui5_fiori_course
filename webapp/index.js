5a36020b1de1d5f7f4664d63a7476b26d90b6a82
Szilágyi István <i10815@eon.com>
2025-03-07 22:11:19 +0100

    new button, in controller

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,11 @@
 sap.ui.define([
-    "sap/m/Text"
-], function (Text) {
+    "sap/ui/core/mvc/XMLView"
+], function (XMLView) {
     "use strict";
 
-    alert("UI5 is ready to go!");
+    XMLView.create({
+        viewName: "sap.ui.demo.walkthrough.view.App"
+    }).then(function (oView) {
+        oView.placeAt("content");
+    });
 });
