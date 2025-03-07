b1656fdd0563d268fecd46f076bfbf2aa5a6e934
Szilágyi István <i10815@eon.com>
2025-03-07 22:39:12 +0100

    i18n setup

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,38 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.App", {
+        onInit: function () {
+            // create JSON model instance
+            const oData = {
+                recipient: {
+                    name: "UI5"
+                }
+            };
+            const oModel = new JSONModel(oData);
+            // set model to the view
+            this.getView().setModel(oModel);
+            // set i18n model on view
+            const i18nModel = new ResourceModel({
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+                supportedLocales: [""],
+                fallbackLocale: ""
+            });
+            this.getView().setModel(i18nModel, "i18n");
+        },
         onShowHello: function () {
-            // show a native JavaScript alert
-            alert("Hello, World!");
+            // show a MessageToast
+            MessageToast.show(this.getView()
+                .getModel("i18n").getResourceBundle()
+                .getText("helloMsg", [this.getView()
+                    .getModel()
+                    .getProperty("/recipient/name")]
+            ));
         }
     });
-})
+});
